3c2c519d516d7f840f7d7be61a2962fabb789227
Luis Pérez <luis.lpv.22@gmail.com>
2025-04-08 18:08:38 +0200

    Default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="font-sans min-h-screen bg-zinc-950 text-white">
+    <header class="fixed top-0 z-10 w-full flex items-center px-14 py-10">
+      <NuxtLink to="/" class="flex items-center gap-2" title="Home">
+        <Logo class="h-10" />
+      </NuxtLink>
+      <div class="flex grow justify-center gap-1">
+        <Chip selected>All</Chip>
+        <Chip>Movies</Chip>
+        <Chip>Series</Chip>
+      </div>
+    </header>
+    <slot />
+  </div>
+</template>
